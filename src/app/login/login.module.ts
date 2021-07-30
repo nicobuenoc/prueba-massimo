037409af00872ef0aee0e10ce98f1413714a860a
Nico <>
2021-07-30 21:10:46 +0200

    Correción en el nombre del routing module del login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginComponentsRoutingModule } from './login-routing.module';
+import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
-  imports: [CommonModule, LoginComponentsRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, LoginRoutingModule, FormsModule, ReactiveFormsModule],
   declarations: [LoginComponent]
 })
 export class LoginModule {}
