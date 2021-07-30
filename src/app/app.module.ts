de2d2a6b65d52c8f6dc4d2f6bd43f693c8f0e565
Nico <>
2021-07-30 21:39:47 +0200

    Componente Principal al modulo principal y no en app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { PrincipalModule } from './principal/principal.module';
 
 // Components
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './components/register/register.component';
-import { PrincipalComponent } from './principal/principal.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
-  declarations: [AppComponent, RegisterComponent, PrincipalComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, PrincipalModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/principal/principal.module.ts
+++ src/app/principal/principal.module.ts
@@ -8,9 +8,10 @@ import { ShipsComponent } from '../components/ships/ships.component';
 import { PageOneComponent } from '../components/page-one/page-one.component';
 import { PageTwoComponent } from '../components/page-two/page-two.component';
 import { ShipsDetailsComponent } from '../components/ships/ships-details/ships-details.component';
+import { PrincipalComponent } from './principal.component';
 
 @NgModule({
-  declarations: [ShipsComponent, ShipsDetailsComponent, PageOneComponent, PageTwoComponent],
+  declarations: [PrincipalComponent, ShipsComponent, ShipsDetailsComponent, PageOneComponent, PageTwoComponent],
   imports: [CommonModule, PrincipalComponentsRoutingModule, HttpClientModule, NgxPaginationModule]
 })
 export class PrincipalModule {}
