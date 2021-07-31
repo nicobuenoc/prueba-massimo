60b43ebdc6b4ea9d4a2c679fc5bbcbd112f8210c
Nico <>
2021-08-01 00:37:03 +0200

    Mejora en el paso de parametros en la llamada get

--- src/app/core/services/ships/ships.service.ts
+++ src/app/core/services/ships/ships.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { StarShipResponse } from 'src/app/core/models/starships-response.model';
 
@@ -17,6 +17,12 @@ export class ShipsService {
   constructor(private http: HttpClient) {}
 
   getShips(page: number): Observable<StarShipResponse> {
-    return this.http.get<StarShipResponse>(`${this.url}${page > 0 ? '/page=' + page : ''}`);
+    let params = new HttpParams();
+
+    if (page > 0) {
+      params = params.set('page', page.toString());
+    }
+
+    return this.http.get<StarShipResponse>(this.url, { params });
   }
 }
