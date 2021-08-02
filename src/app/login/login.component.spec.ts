425c6a886c8ae11b95417105e2f6ea5434fea6e1
Nico <>
2021-08-02 20:31:21 +0200

    Test login, se añade el mock de usersService en el providers

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { UsersService } from '../core/services/users/users.service';
 
 import { LoginComponent } from './login.component';
 
@@ -10,11 +11,17 @@ describe('LoginComponent', () => {
   let fixture: ComponentFixture<LoginComponent>;
   let router: Router;
 
+  const usersServiceMock = {
+    existsUser() {},
+    registerUser() {}
+  };
+
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
         declarations: [LoginComponent],
-        imports: [FormsModule, RouterTestingModule.withRoutes([]), ReactiveFormsModule]
+        imports: [FormsModule, RouterTestingModule.withRoutes([]), ReactiveFormsModule],
+        providers: [{ provide: UsersService, useFactory: () => usersServiceMock }]
       }).compileComponents();
     })
   );
