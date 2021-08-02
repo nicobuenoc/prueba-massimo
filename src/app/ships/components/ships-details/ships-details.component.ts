e94ea2e6965b7d108e2e2bea81e1e8f3f877acd0
Nico <>
2021-08-03 01:08:34 +0200

    A partir de la url que contiene el id creamos la url para visualizar la imagen correspodiente de las naves

--- src/app/ships/components/ships-details/ships-details.component.ts
+++ src/app/ships/components/ships-details/ships-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Ship } from 'src/app/core/models/ship.model';
 import { StarShipResponse } from 'src/app/core/models/starships-response.model';
+import { environment } from 'src/environments/environment';
 declare var $: any;
 
 @Component({
@@ -30,10 +31,11 @@ export class ShipsDetailsComponent implements OnInit {
     this.config.currentPage = 1;
   }
 
-  getStarshipId(url) {
+  getStarshipUrlImage(url: string) {
     this.shipId = url.slice(0, -1);
-    const urlImage = `${this.shipId}.jpg`;
-    return urlImage !== '';
+    this.shipId = this.shipId.substring(this.shipId.lastIndexOf('/') + 1, this.shipId.length);
+    const urlImage = `${environment.urlImagenStarships}${this.shipId}.jpg`;
+    return urlImage || '';
   }
 
   pageChanged(event: number) {
